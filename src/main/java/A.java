5e3d067e350f7b5656338a6a9c4b7b44e7b84bc4
cq <2437024718@qq.com>
2022-12-26 19:12:35 +0800

    qweqweqwewq

---
+++ src/main/java/A.java
@@ -0,0 +1,9 @@
+/**
+ * @Author 陈强
+ * @Date 2022/12/26 18:49
+ */
+public class A {
+    public static void main(String[] args) {
+        System.out.println("AAA");
+    }
+}
---
+++ src/main/java/B.java
@@ -0,0 +1,10 @@
+/**
+ * @Author 陈强
+ * @Date 2022/12/26 18:50
+ */
+public class B {
+    public static void main(String[] args) {
+        System.out.println("BBB");
+        System.out.println("BBB");
+    }
+}
